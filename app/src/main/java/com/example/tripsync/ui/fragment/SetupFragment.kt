6c08d90652c87209e7382ef6d563881d0571dbac
Ethan <fanfirst4@naver.com>
2023-10-12 12:18:15 +0900

    [Design] : SetupFragment UI

--- app/src/main/java/com/example/tripsync/ui/fragment/SetupFragment.kt
+++ app/src/main/java/com/example/tripsync/ui/fragment/SetupFragment.kt
@@ -1,11 +1,14 @@
 package com.example.tripsync.ui.fragment
 
+import android.app.DatePickerDialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
 import com.example.tripsync.databinding.FragmentSetupBinding
+import java.util.Calendar
 
 class SetupFragment : Fragment() {
 
@@ -18,17 +21,33 @@ class SetupFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentSetupBinding.inflate(inflater, container, false)
-        return binding.root
-    }
+        val view = binding.root
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        //doSomething
+        val selectDateButton = binding.appCompatButton // Assuming you have a button with this ID in your XML layout
+
+        selectDateButton.setOnClickListener {
+            showDatePicker()
+        }
+
+        return view
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun showDatePicker() {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(requireContext(), { _, year, month, dayOfMonth ->
+            val selectedDate = "$year-${month + 1}-$dayOfMonth" // Format the selected date as needed
+            // You can use 'selectedDate' as per your requirements
+        }, year, month, day)
+        datePickerDialog.show()
     }
 
     companion object {
@@ -37,3 +56,8 @@ class SetupFragment : Fragment() {
         }
     }
 }
+
+
+
+
+
