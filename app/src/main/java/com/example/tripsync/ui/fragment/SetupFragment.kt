f614a89dce482a3fbbed1be65f5c160e3db80f5e
Ethan <fanfirst4@naver.com>
2023-10-12 16:44:53 +0900

    [design] : PlanFragment Layout

--- app/src/main/java/com/example/tripsync/ui/fragment/SetupFragment.kt
+++ app/src/main/java/com/example/tripsync/ui/fragment/SetupFragment.kt
@@ -23,7 +23,7 @@ class SetupFragment : Fragment() {
         _binding = FragmentSetupBinding.inflate(inflater, container, false)
         val view = binding.root
 
-        val selectDateButton = binding.appCompatButton // Assuming you have a button with this ID in your XML layout
+        val selectDateButton = binding.appCompatButton
 
         selectDateButton.setOnClickListener {
             showDatePicker()
@@ -44,8 +44,7 @@ class SetupFragment : Fragment() {
         val day = calendar.get(Calendar.DAY_OF_MONTH)
 
         val datePickerDialog = DatePickerDialog(requireContext(), { _, year, month, dayOfMonth ->
-            val selectedDate = "$year-${month + 1}-$dayOfMonth" // Format the selected date as needed
-            // You can use 'selectedDate' as per your requirements
+            val selectedDate = "$year-${month + 1}-$dayOfMonth"
         }, year, month, day)
         datePickerDialog.show()
     }
