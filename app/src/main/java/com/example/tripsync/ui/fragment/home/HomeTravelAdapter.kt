7daf9acfbabf5131e0f0a04b614c04ef1fc54eab
leedaeul <ouowinnie@gmail.com>
2023-10-17 19:46:34 +0900

    [FEAT] : 여행 API 기능 구현

--- app/src/main/java/com/example/tripsync/ui/fragment/home/HomeTravelAdapter.kt
+++ app/src/main/java/com/example/tripsync/ui/fragment/home/HomeTravelAdapter.kt
@@ -2,13 +2,19 @@ package com.example.tripsync.ui.fragment.home
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.tripsync.R
 import com.example.tripsync.databinding.TravelItemBinding
+import com.example.tripsync.model.Travel
 
-class HomeTravelAdapter(private var items: MutableList<Pair<Any, String>>): RecyclerView.Adapter<HomeTravelAdapter.ViewHolder>() {
+class HomeTravelAdapter(private var items: List<Travel>): RecyclerView.Adapter<HomeTravelAdapter.ViewHolder>() {
+
+    fun updateItems(newItems: List<Travel>) {
+        items = newItems
+        notifyDataSetChanged()
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeTravelAdapter.ViewHolder {
         val binding = TravelItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -16,9 +22,8 @@ class HomeTravelAdapter(private var items: MutableList<Pair<Any, String>>): Recy
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val (itemImage, itemText) = items[position]
-        holder.bindImageResource(itemImage as Int)
-        holder.bindTextItem(itemText)
+        val item = items[position]
+        holder.setItem(item)
     }
 
     override fun getItemCount(): Int {
@@ -26,13 +31,13 @@ class HomeTravelAdapter(private var items: MutableList<Pair<Any, String>>): Recy
     }
 
     inner class ViewHolder(private val binding: TravelItemBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bindImageResource(imageRes: Int) {
-            val imageView = binding.travelItemImg
-            imageView.setImageResource(imageRes)
-        }
-        fun bindTextItem(text: String) {
-            val textView = binding.travelItemText
-            textView.text = text
+        fun setItem(item: Travel) {
+            val image = binding.travelItemImg
+            val title = binding.travelItemText
+            Glide.with(binding.root)
+                .load(item.imageUrl)
+                .into(image)
+            title.text = item.title
         }
     }
 }
