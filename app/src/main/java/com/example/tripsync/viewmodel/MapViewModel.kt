6bc6940daf67d06af84fc61d279ef192196ef177
tastystar97 <63907062+tastystar97@users.noreply.github.com>
2023-10-20 20:59:40 +0900

    [FEAT] : 디테일 프래그먼트 기능 구현
    디테일 프래그먼트 기능 구현 완료
    - 맵 마커
    - 북마크
    - 찜 목록 이동

---
+++ app/src/main/java/com/example/tripsync/viewmodel/MapViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.tripsync.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MapViewModel : ViewModel() {
+
+    var mapXY = MutableLiveData<Pair<Double, Double>> (0.0 to 0.0)
+}
