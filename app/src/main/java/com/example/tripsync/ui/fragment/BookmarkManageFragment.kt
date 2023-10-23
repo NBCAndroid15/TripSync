bf8c1ae0213fbc3ae9e4ed32f675147d6675a20b
leedaeul <ouowinnie@gmail.com>
2023-10-23 20:57:49 +0900

    [FEAT] : 마이페이지 뷰페이저 기능 구현

--- app/src/main/java/com/example/tripsync/ui/fragment/BookmarkManageFragment.kt
+++ app/src/main/java/com/example/tripsync/ui/fragment/BookmarkManageFragment.kt
@@ -12,6 +12,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tripsync.databinding.FragmentBookmarkManageBinding
 import com.example.tripsync.model.Travel
+import com.example.tripsync.ui.adapter.BookmarkAreaAdapter
 import com.example.tripsync.ui.adapter.BookmarkManageAdapter
 import com.example.tripsync.ui.dialog.ConfirmDialog
 import com.example.tripsync.viewmodel.BookmarkManageViewModel
@@ -19,6 +20,7 @@ import com.example.tripsync.viewmodel.BookmarkManageViewModelFactory
 
 class BookmarkManageFragment : Fragment() {
 
+    private lateinit var bookmarkAreaAdapter: BookmarkAreaAdapter
     private var _binding: FragmentBookmarkManageBinding? = null
     private val binding: FragmentBookmarkManageBinding
         get() = _binding!!
@@ -51,6 +53,19 @@ class BookmarkManageFragment : Fragment() {
         binding.bookmarkManageBookmarkRv.adapter = adapter
         binding.bookmarkManageBookmarkRv.layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
         binding.bookmarkManageBookmarkRv.itemAnimator = null
+
+        // 지역
+        val areaList = mutableListOf("전체", "서울", "인천", "대전", "대구", "광주", "부산", "울산", "세종", "경기", "강원", "충북", "충남", "경북", "경남", "전북", "전남", "제주")
+        bookmarkAreaAdapter = BookmarkAreaAdapter(areaList)
+        binding.bookmarkCategoryRv.adapter = bookmarkAreaAdapter
+        binding.bookmarkCategoryRv.layoutManager = LinearLayoutManager(requireContext(),
+            LinearLayoutManager.HORIZONTAL,false)
+
+        bookmarkAreaAdapter.itemClick = object : BookmarkAreaAdapter.ItemClick {
+            override fun onClick(keyword: String) {
+                //travelViewModel.updateKeyword(keyword)
+            }
+        }
 
         viewModel.bookmarkList.observe(viewLifecycleOwner) {
             adapter.setList(it)
