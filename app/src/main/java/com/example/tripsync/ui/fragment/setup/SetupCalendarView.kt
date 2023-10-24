f3f92c23c6236aa09fe9138e94fbc1b9ed1af851
Ethan <fanfirst4@naver.com>
2023-10-24 09:32:12 +0900

    [FEAT] 캘린더

--- app/src/main/java/com/example/tripsync/ui/fragment/setup/SetupCalendarView.kt
+++ app/src/main/java/com/example/tripsync/ui/fragment/setup/SetupCalendarView.kt
@@ -7,18 +7,22 @@ import androidx.fragment.app.DialogFragment
 import com.example.tripsync.R
 import com.example.tripsync.databinding.SetupCalendarviewBinding
 import com.prolificinteractive.materialcalendarview.CalendarDay
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView
 import com.prolificinteractive.materialcalendarview.format.ArrayWeekDayFormatter
+import java.util.Calendar
 
 
 class SetupCalendarView(
-                        private val onDateSelected: (Set<CalendarDay>) -> Unit ) : DialogFragment() {
+    private val onDateSelected: (Set<CalendarDay>) -> Unit
+) : DialogFragment() {
 
     private lateinit var binding: SetupCalendarviewBinding
-    private var temporarySelectedDates = mutableSetOf<CalendarDay>()
-    private val selectedDates = mutableSetOf<CalendarDay>()
 
-
-
+    // 선택한 날짜를 저장
+    private var selectedDates = mutableSetOf<CalendarDay>()
+    private var isRangeSelecting = false
+    private var startDate: CalendarDay? = null
+    private var endDate: CalendarDay? = null
 
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
@@ -28,14 +32,10 @@ class SetupCalendarView(
 
         initView()
         binding.okayBtn.setOnClickListener {
-            selectedDates.clear()
-            selectedDates.addAll(temporarySelectedDates)
+
             onDateSelected(selectedDates)
             dialog.dismiss()
-            dialog.dismiss()
         }
-
-
 
         return dialog
     }
@@ -46,39 +46,44 @@ class SetupCalendarView(
         setupCalendar.setWeekDayFormatter(ArrayWeekDayFormatter(resources.getTextArray(R.array.custom_weekdays)))
         setupCalendar.setHeaderTextAppearance(R.style.CalendarWidgetHeader)
 
-        setupCalendar.setOnRangeSelectedListener { widget, dates ->
-            temporarySelectedDates = dates.toMutableSet()
+        setupCalendar.setOnDateChangedListener { _, date, selected ->
+            if (selected) {
+                if (isRangeSelecting) {
+                    val datesInRange = findDatesInRange(startDate!!, date)
+                    selectedDates.addAll(datesInRange)
+                    isRangeSelecting = false
+                } else {
+                    selectedDates.clear()
+                    startDate = date
+                    selectedDates.add(date)
+                    isRangeSelecting = true
+                }
+            } else {
+                selectedDates.remove(date)
+            }
         }
-//        setupCalendar.setWeekDayFormatter(ArrayWeekDayFormatter(resources.getTextArray(R.array.custom_weekdays)))
-//        setupCalendar.setHeaderTextAppearance(R.style.CalendarWidgetHeader)
-//
-//        setupCalendar.setOnRangeSelectedListener { widget, dates ->
-//            val selectedDate = widget.selectedDates
-//            (parentFragment as? SetupFragment)?.onDateSelected(selectedDate.toSet())
-//            val distinctSelectedDates = if (selectedDate.isNotEmpty()) {
-//                val startDate = selectedDate[0]
-//                val endDate = selectedDate[selectedDate.size - 1]
-//                if (startDate == endDate) {
-//                    setOf(startDate)
-//                } else {
-//                    selectedDate.toSet()
-//                }
-//            } else {
-//                emptySet()
-//            }
-//            doSomethingWithDistinctDates(distinctSelectedDates)
-//            temporarySelectedDates.clear()
-//            temporarySelectedDates.addAll(selectedDate)        }
-    }
-    private fun doSomethingWithDistinctDates(distinctSelectedDates: Set<CalendarDay>) {
-        for (date in distinctSelectedDates) {
-            val year = date.year
-            val month = date.month
-            val day = date.day
-            Log.d("SetupCalendarView", "Selected Date: $year-$month-$day")
-        }
-
     }
 
+    private fun findDatesInRange(startDate: CalendarDay, endDate: CalendarDay): List<CalendarDay> {
+        val datesInRange = mutableListOf<CalendarDay>()
+        var currentDay = startDate
 
+        while (!currentDay.isAfter(endDate)) {
+            datesInRange.add(currentDay)
+            currentDay = currentDay.plusDays(1)
+        }
+        return datesInRange
+    }
+
+}
+
+private fun CalendarDay.plusDays(days: Int): CalendarDay {
+    val calendar = Calendar.getInstance()
+    calendar.set(year, month - 1, day)
+    calendar.add(Calendar.DAY_OF_MONTH, days)
+    val newYear = calendar.get(Calendar.YEAR)
+    val newMonth = calendar.get(Calendar.MONTH) + 1
+    val newDay = calendar.get(Calendar.DAY_OF_MONTH)
+
+    return CalendarDay.from(newYear, newMonth, newDay)
 }
