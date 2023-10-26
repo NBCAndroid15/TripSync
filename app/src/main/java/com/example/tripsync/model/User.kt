bafd131e7da19d71dd61b05753dfe7c8dd8de437
bkt2790 <qwert9788@hanyang.ac.kr>
2023-10-26 21:13:23 +0900

    [FEAT] : profileImg

--- app/src/main/java/com/example/tripsync/model/User.kt
+++ app/src/main/java/com/example/tripsync/model/User.kt
@@ -4,5 +4,6 @@ data class User (
     val email : String? = null,
     val nickname : String? = null,
     val friends : List<User>? = null,
-    val uid : String? = null
+    val uid : String? = null,
+    val profileImg: String? = null
 )
