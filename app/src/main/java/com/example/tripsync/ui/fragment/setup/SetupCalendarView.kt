5c8d4c5e5fbff63496a336156abca23a8833d73a
Ethan <fanfirst4@naver.com>
2023-10-27 17:08:51 +0900

    [Desing] : Font 추가

--- app/src/main/java/com/example/tripsync/ui/fragment/setup/SetupCalendarView.kt
+++ app/src/main/java/com/example/tripsync/ui/fragment/setup/SetupCalendarView.kt
@@ -1,7 +1,11 @@
 package com.example.tripsync.ui.fragment.setup
 
 import android.app.Dialog
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.style.ForegroundColorSpan
+import android.text.style.RelativeSizeSpan
+import android.text.style.StyleSpan
 import android.view.View
 import android.view.WindowManager
 import androidx.core.util.Pair
@@ -9,9 +13,12 @@ import androidx.fragment.app.DialogFragment
 import com.example.tripsync.R
 import com.example.tripsync.databinding.SetupCalendarviewBinding
 import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.type.Color
 import com.prolificinteractive.materialcalendarview.CalendarDay
+import com.prolificinteractive.materialcalendarview.DayViewDecorator
+import com.prolificinteractive.materialcalendarview.DayViewFacade
 import com.prolificinteractive.materialcalendarview.format.ArrayWeekDayFormatter
-
+import java.util.Calendar
 
 
 class SetupCalendarView(
@@ -29,61 +36,85 @@ class SetupCalendarView(
         val dialog = Dialog(requireContext())
         dialog.setContentView(binding.root)
 
-//        initView()
-//        binding.okayBtn.setOnClickListener {
-//
-//            onDateSelected(selectedDates)
-//            dialog.dismiss()
-//        }
+        initView()
+        binding.okayBtn.setOnClickListener {
 
-        initDatePicker()
+            onDateSelected(selectedDates)
+            dialog.dismiss()
+        }
+
 
         return dialog
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
 
-        val width = WindowManager.LayoutParams.MATCH_PARENT
-        val height = 300
 
-        dialog?.window?.setLayout(width, height)
+    private fun initView() = with(binding) {
+
+        setupCalendar.setWeekDayFormatter(ArrayWeekDayFormatter(resources.getTextArray(R.array.custom_weekdays)))
+        setupCalendar.setHeaderTextAppearance(R.style.CalendarWidgetHeader)
+
+        setupCalendar.setOnDateChangedListener { _, date, selected ->
+            if (selected) {
+                selectedDates.clear()
+                selectedDates.add(date)
+            } else {
+                selectedDates.remove(date)
+            }
+        }
+
+        setupCalendar.setOnRangeSelectedListener { _, dates ->
+            selectedDates.clear()
+            selectedDates.addAll(dates)
+        }
+
+
     }
 
-
-//    private fun initView() = with(binding) {
+//    class TodayDecorator: DayViewDecorator {
+//        private var date = CalendarDay.today()
 //
-//        setupCalendar.setWeekDayFormatter(ArrayWeekDayFormatter(resources.getTextArray(R.array.custom_weekdays)))
-//        setupCalendar.setHeaderTextAppearance(R.style.CalendarWidgetHeader)
-//
-//        setupCalendar.setOnDateChangedListener { _, date, selected ->
-//            if (selected) {
-//                selectedDates.add(date)
-//            } else {
-//                selectedDates.remove(date)
-//            }
+//        override fun shouldDecorate(day: CalendarDay?): Boolean {
+//            return day?.equals(date)!!
 //        }
 //
-//        setupCalendar.setOnRangeSelectedListener { widget, dates ->
-//            selectedDates.clear()
-//            selectedDates.addAll(dates)
+//        override fun decorate(view: DayViewFacade?) {
+//            view?.addSpan(StyleSpan(Typeface.BOLD))
+//            view?.addSpan(RelativeSizeSpan(1.4f))
+//            view?.addSpan(ForegroundColorSpan(Color.parseColor("#1D872A")))
+//        }
+//    }
+//
+//
+//    class SatDecorator: DayViewDecorator {
+//        private val calendar = Calendar.getInstance()
+//
+//        override fun shouldDecorate(day: CalendarDay?): Boolean {
+//            day?.copyTo(calendar)
+//            val weekDay = calendar[Calendar.DAY_OF_WEEK]
+//            return weekDay == Calendar.SATURDAY
 //        }
 //
+//        override fun decorate(view: DayViewFacade?) {
+//            view?.addSpan(ForegroundColorSpan(R.color.main_defalt))
+//        }
+//    }
+//
+//    class SunDecorator: DayViewDecorator {
+//        private val calendar = Calendar.getInstance()
+//
+//        override fun shouldDecorate(day: CalendarDay?): Boolean {
+//            day?.copyTo(calendar)
+//            val weekDay = calendar[Calendar.DAY_OF_WEEK]
+//            return weekDay == Calendar.SUNDAY
+//        }
+//
+//        override fun decorate(view: DayViewFacade?) {
+//            view?.addSpan(ForegroundColorSpan(Color.RED))
+//        }
 //    }
 
-    private fun initDatePicker()= with(binding) {
-        val dateRangePicker = MaterialDatePicker.Builder.dateRangePicker()
-            .setTitleText("여행")
-            .setSelection(
-                Pair(
-                    MaterialDatePicker.thisMonthInUtcMilliseconds(),
-                    MaterialDatePicker.todayInUtcMilliseconds()
-                )
-            )
-            .build()
 
-        dateRangePicker.show(parentFragmentManager, "tag")
-    }
 
 
 }
