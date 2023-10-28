72e84b8bc34fb469a29807ec77ab95246c294fb3
Ethan <fanfirst4@naver.com>
2023-10-28 14:37:00 +0900

    [Design] :

--- app/src/main/java/com/example/tripsync/ui/fragment/plan/PlanListAdapter.kt
+++ app/src/main/java/com/example/tripsync/ui/fragment/plan/PlanListAdapter.kt
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.tripsync.R
 import com.example.tripsync.databinding.PlanRecyclerItemBinding
 import com.example.tripsync.model.Travel
 
@@ -38,6 +39,7 @@ class PlanListAdapter(private val onItemRemove: (Travel) -> Unit): ListAdapter<T
         fun bind(item: Travel) = with(binding) {
             Glide.with(itemView)
                 .load(item.imageUrl)
+                .error(R.drawable.defalt_profile)
                 .into(planItemImage)
 
             planItemTitle.text = item.title
