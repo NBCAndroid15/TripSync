4f13256943ef67dffc1b9b565a0960d624316922
tastystar97 <63907062+tastystar97@users.noreply.github.com>
2023-11-06 15:53:23 +0900

    [FEAT] : 비회원 기능
    비회원이 홈에서 계획 버튼 클릭 시 로그인 프래그먼트로 이동

--- app/src/main/java/com/example/tripsync/ui/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/example/tripsync/ui/fragment/home/HomeFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -13,12 +14,14 @@ import com.example.tripsync.R
 import com.example.tripsync.databinding.FragmentHomeBinding
 import com.example.tripsync.model.Travel
 import com.example.tripsync.ui.fragment.DetailFragment
+import com.example.tripsync.ui.fragment.LoginFragment
 import com.example.tripsync.ui.fragment.setup.SetupFragment
 import com.example.tripsync.ui.fragment.setup.SharedViewModel
 import com.example.tripsync.viewmodel.FestivalViewModel
 import com.example.tripsync.viewmodel.FestivalViewModelFactory
 import com.example.tripsync.viewmodel.TravelViewModel
 import com.example.tripsync.viewmodel.TravelViewModelFactory
+import com.google.firebase.auth.FirebaseAuth
 
 class HomeFragment : Fragment(),
     HomeFestivalAdapter.onFestivalClick, HomeTravelAdapter.onTravelClick {
@@ -29,6 +32,7 @@ class HomeFragment : Fragment(),
     private val travelViewModel: TravelViewModel by activityViewModels { TravelViewModelFactory() }
     private val festivalViewModel: FestivalViewModel by activityViewModels { FestivalViewModelFactory() }
     private val sharedViewModel: SharedViewModel by activityViewModels()
+    private var isUserLogIn: Boolean = false
 
 
     private var _binding: FragmentHomeBinding? = null
@@ -42,15 +46,32 @@ class HomeFragment : Fragment(),
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
 
 
-
-
-
         // 지역
-        val areaList = mutableListOf("전체", "서울", "인천", "대전", "대구", "광주", "부산", "울산", "세종", "경기", "강원", "충북", "충남", "경북", "경남", "전북", "전남", "제주")
+        val areaList = mutableListOf(
+            "전체",
+            "서울",
+            "인천",
+            "대전",
+            "대구",
+            "광주",
+            "부산",
+            "울산",
+            "세종",
+            "경기",
+            "강원",
+            "충북",
+            "충남",
+            "경북",
+            "경남",
+            "전북",
+            "전남",
+            "제주"
+        )
         homeAreaAdapter = HomeAreaAdapter(areaList)
         binding.homeCategoryRv.adapter = homeAreaAdapter
         homeAreaAdapter.itemStates[0] = true
-        binding.homeCategoryRv.layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.HORIZONTAL,false)
+        binding.homeCategoryRv.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
 
         homeAreaAdapter.itemClick = object : HomeAreaAdapter.ItemClick {
             override fun onClick(keyword: String) {
@@ -60,23 +81,43 @@ class HomeFragment : Fragment(),
 
         // 여행
         binding.homeTravelRv.adapter = homeTravelAdapter
-        binding.homeTravelRv.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL,false)
+        binding.homeTravelRv.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
 
         // 축제
         binding.homeFestivalRv.adapter = homeFestivalAdapter
-        binding.homeFestivalRv.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL,false)
+        binding.homeFestivalRv.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+
 
         binding.homeStartplanBtn.setOnClickListener {
+            if (isUserLogIn()) {
+                requireActivity().supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_frame, SetupFragment())
+                    .addToBackStack(null)
+                    .commit()
+
+                // 홈에서 setup으로 이동할 때 setup 버튼들의 가시성 초기화
+                sharedViewModel.setTitleVisible(false)
+                sharedViewModel.setUserVisible(false)
+                sharedViewModel.setUserCheck(false)
+
+            } else {
+                Toast.makeText(requireContext(), "로그인이 필요한 서비스에요!", Toast.LENGTH_SHORT).show()
+                requireActivity().supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_frame, LoginFragment())
+                    .addToBackStack(null)
+                    .commit()
+            }
+        }
+
+        binding.homeLoginTv.setOnClickListener() {
             requireActivity().supportFragmentManager.beginTransaction()
-                .replace(R.id.main_frame, SetupFragment())
+                .replace(R.id.main_frame, LoginFragment())
                 .addToBackStack(null)
                 .commit()
-
-            // 홈에서 setup으로 이동할 때 setup 버튼들의 가시성 초기화
-            sharedViewModel.setTitleVisible(false)
-            sharedViewModel.setUserVisible(false)
-            sharedViewModel.setUserCheck(false)
         }
+
 
         return binding.root
     }
@@ -84,7 +125,7 @@ class HomeFragment : Fragment(),
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        homeTravelAdapter.setOntravelClickListener (this)
+        homeTravelAdapter.setOntravelClickListener(this)
         homeFestivalAdapter.setOnFestivalClickListener(this)
 
         // 여행 아이템 목록 업데이트
@@ -105,12 +146,28 @@ class HomeFragment : Fragment(),
             }
         }
 
+        val isUserLoggedIn = isUserLogIn()
+
+        if (isUserLoggedIn) {
+            binding.homeLoginTv.visibility = View.GONE
+        } else {
+            binding.homeLoginTv.visibility = View.VISIBLE
+        }
+
+        homeTravelAdapter.setOntravelClickListener(this)
+        homeFestivalAdapter.setOnFestivalClickListener(this)
+
     }
 
     override fun onTravelClick(travel: Travel) {
         val fragment = DetailFragment(travel)
         requireActivity().supportFragmentManager.beginTransaction()
-            .setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left, R.anim.enter_from_left,R.anim.exit_to_right)
+            .setCustomAnimations(
+                R.anim.enter_from_right,
+                R.anim.exit_to_left,
+                R.anim.enter_from_left,
+                R.anim.exit_to_right
+            )
             .add(R.id.main_frame, fragment)
             .addToBackStack(null)
             .commit()
@@ -119,10 +176,22 @@ class HomeFragment : Fragment(),
     override fun onFestivalClick(travel: Travel) {
         val fragment = DetailFragment(travel)
         requireActivity().supportFragmentManager.beginTransaction()
-            .setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left, R.anim.enter_from_left,R.anim.exit_to_right)
+            .setCustomAnimations(
+                R.anim.enter_from_right,
+                R.anim.exit_to_left,
+                R.anim.enter_from_left,
+                R.anim.exit_to_right
+            )
             .add(R.id.main_frame, fragment)
             .addToBackStack(null)
             .commit()
+    }
+
+    private fun isUserLogIn(): Boolean {
+        val auth = FirebaseAuth.getInstance()
+        val currentUser = auth.currentUser
+
+        return currentUser != null
     }
 
     override fun onDestroyView() {
